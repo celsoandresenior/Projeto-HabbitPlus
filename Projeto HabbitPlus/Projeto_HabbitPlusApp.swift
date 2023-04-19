9135bcaba40ad7edd3855596f5af39721dbd51fb
celso lima <celsolog2012@icloud.com>
2023-04-19 16:47:20 -0300

    preparando o projeto

--- Projeto HabbitPlus/Projeto_HabbitPlusApp.swift
+++ Projeto HabbitPlus/Projeto_HabbitPlusApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Projeto_HabbitPlusApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashView()
         }
     }
 }
---
+++ Projeto HabbitPlus/Splash/ViewModel/SplashViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SplashViewModel.swift
+//  Projeto HabbitPlus
+//
+//  Created by Celso Lima on 19/04/23.
+//
+
+import Foundation
