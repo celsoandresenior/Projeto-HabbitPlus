6d7a6a06263841fbe7a26a94a2e2fdf406822573
celso lima <celsolog2012@icloud.com>
2023-04-19 16:26:41 -0300

    Initial Commit

---
+++ Projeto HabbitPlus/Projeto_HabbitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projeto_HabbitPlusApp.swift
+//  Projeto HabbitPlus
+//
+//  Created by Celso Lima on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Projeto_HabbitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projeto HabbitPlusUITests/Projeto_HabbitPlusUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Projeto_HabbitPlusUITestsLaunchTests.swift
+//  Projeto HabbitPlusUITests
+//
+//  Created by Celso Lima on 19/04/23.
+//
+
+import XCTest
+
+final class Projeto_HabbitPlusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
